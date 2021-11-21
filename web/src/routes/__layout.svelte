0474e8ce7210552ad7488f44636d8d94607e9813
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-11-21 06:05:41 +0300

    favicon

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -7,6 +7,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Cousine:wght@400;700&family=Nunito+Sans:wght@400;700;900&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'><text x='0' y='14'>📈📉</text></svg>" />
 </svelte:head>
 <main class="w-full max-w-3xl mx-auto">
   <Navbar/>
