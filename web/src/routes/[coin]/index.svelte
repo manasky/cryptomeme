622c2238fec47c96419eb3b6c6be7ab86ade555a
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-11-22 05:58:25 +0300

    coin page

--- web/src/routes/[coin]/index.svelte
+++ web/src/routes/[coin]/index.svelte
@@ -1,10 +1,12 @@
 <script>
 import { onMount } from 'svelte'
 import { page } from '$app/stores';
-import { formatPrice } from '@src/functions/numbers.js'
+import { formatPrice } from '@src/functions/utils.js'
 import Seo from '@components/Seo.svelte'
 import Meme from '@components/Meme.svelte'
-
+import sparkline from "@fnando/sparkline"
+import { normalizeArrayToMin, extractObjectProperyToArray } from '@src/functions/utils.js'
+import { timeAgo } from '@src/functions/timeago.js'
 
 let API_PATH
 if (import.meta.env.VITE_API_PATH !== undefined) {
@@ -13,22 +15,24 @@ if (import.meta.env.VITE_API_PATH !== undefined) {
 
 let coin
 let coins
-let lastUpdate
 let loadingStatus = 'idle'
+let sparklineEl
+let chartValues
 onMount(() => {
 
   function getCoins() {
     loadingStatus = 'loading'
     coins = JSON.parse(localStorage.getItem('coins'))
-    lastUpdate = localStorage.getItem('lastUpdate')
+    coin = coins.find(element => element.symbol === $page.params.coin)
     fetch(API_PATH+'/markets')
       .then((response) => response.json())
       .then((response) => {
         loadingStatus = 'success'
         coins = response
         localStorage.setItem('coins', JSON.stringify(coins));
-        localStorage.setItem('lastUpdate', new Date().toISOString());
-        coin = coins.find(element => element.symbol === $page.params.coin)
+        
+        chartValues = extractObjectProperyToArray(coin.prices_7d, 'price')
+        sparkline(sparklineEl, normalizeArrayToMin(chartValues))
       })
       .catch((error) => {
         loadingStatus = 'error'
@@ -58,10 +62,10 @@ onMount(() => {
 </svelte:head>
 
 {#if coin}
-  <div class="flex gap-12 flex-col-reverse items-center md:flex-row">
-    <div class="w-full md:w-80 rounded-box flex flex-col gap-4">
+  <div class="flex gap-14 flex-col-reverse items-center md:flex-row md:items-start">
+    <div class="w-full md:max-w-[20rem] rounded-box flex flex-col gap-8 mx-auto max-w-[85vw]">
 
-      <div class="flex gap-2 items-center w-full px-4">
+      <div class="flex gap-2 items-center w-full">
         <img class="w-12 h-12 rounded" src={coin.image} alt={coin.name} width="48" height="48"/>
         <div>
           <h1 class="font-black text-xl">{coin.name}</h1>
@@ -89,17 +93,37 @@ onMount(() => {
       </div>
 
       <div class="px-4"> 
+        <div class="font-bold text-sm flex items-center gap-2 mb-2">7 day price change</div>
+        <div class="w-full">
+          <svg 
+            bind:this={sparklineEl} 
+            class="mx-auto max-w-full" 
+            width="220" 
+            height="100" 
+            stroke-width="2" 
+            fill="none" 
+            stroke="hsla(var({coin.prices_7d[0].price > coin.prices_7d[coin.prices_7d.length - 1].price ? '--er' : '--su'}))"
+          >
+          </svg>
+        </div>
+      </div>
+
+      <div class="px-4"> 
         <div class="font-bold text-sm flex items-center gap-2 mb-2">Today price range</div>
         <div class="relative pt-8">
           <div class="tooltip absolute tooltip-open tooltip-primary" data-tip="${formatPrice(coin.current_price)}" style="left:{
-            ((coin.high_24h - coin.current_price)*100)/(coin.high_24h - coin.low_24h)
+            Math.min(
+              ((coin.high_24h - coin.current_price)*100)/(coin.high_24h - coin.low_24h)
+            , 100)
             }%">
             <div class="w-px"></div>
           </div>
         </div>
         <div class="h-2 p-px flex items-stretch bg-base-content bg-opacity-20 rounded-full">
           <div class="bg-base-content rounded-full" style="width:{
-            ((coin.high_24h - coin.current_price)*100)/(coin.high_24h - coin.low_24h)
+            Math.min(
+              ((coin.high_24h - coin.current_price)*100)/(coin.high_24h - coin.low_24h)
+            , 100)
             }%"></div>
         </div>
         <div class="flex justify-between py-2">
@@ -111,7 +135,11 @@ onMount(() => {
       <div class="px-4"> 
         <div class="font-bold text-sm flex items-center gap-2 mb-2">All time range</div>
         <div class="relative pt-8">
-          <div class="tooltip absolute tooltip-open tooltip-primary" data-tip="${formatPrice(coin.current_price)}" style="left:{100 - Number(Math.abs(coin.ath_change_percentage))}%">
+          <div class="tooltip absolute tooltip-open tooltip-primary" data-tip="${formatPrice(coin.current_price)}" style="left:{
+            Math.min(
+              100 - Number(Math.abs(coin.ath_change_percentage))
+            , 100)
+            }%">
             <div class="w-px"></div>
           </div>
         </div>
@@ -130,20 +158,35 @@ onMount(() => {
         <div class="font-mono text-sm">${formatPrice(coin.market_cap)}</div>
       </div>
 
+      <div class="px-4"> 
+        <div class="font-bold text-sm flex items-center gap-2 mb-2">Circulating Supply</div>
+        <div class="font-mono text-sm">{coin.circulating_supply}</div>
+      </div>
+
+      <!-- <div class="px-4"> 
+        <div class="font-bold text-sm flex items-center gap-2 mb-2">Last update</div>
+        <div class="font-mono text-sm">{timeAgo(coin.last_updated, true)} ago</div>
+      </div> -->
+
+
+
+
     </div>
     <div>
       {#if coin.meme}
-        <div class="mt-3">
-          <Meme 
-            id={coin.symbol} 
-            coin_name={coin.name} 
-            meme={coin.meme} 
-            meme_caption={coin.meme_caption} 
-            creditDate=""
-            creditPrice= "{coin.symbol.toUpperCase()} price on {new Date(Date.now()).toLocaleString("en-US",{ year: "numeric", month: "short", day: "2-digit" })}: ${coin.current_price} ({(coin.price_change_24h<0?"▼":"▲") + '$' + Number(Math.abs(coin.price_change_24h))})"
-            classes='w-full rounded-box'
-            clickToDownload=true
-          />
+        <div class="tooltip tooltip-open tooltip-bottom" data-tip="↑ click to download image">
+          <div class="my-3">
+            <Meme 
+              id={coin.symbol} 
+              coin_name={coin.name} 
+              meme={coin.meme} 
+              meme_caption={coin.meme_caption} 
+              creditDate=""
+              creditPrice= "{coin.symbol.toUpperCase()} price on {new Date(Date.now()).toLocaleString("en-US",{ year: "numeric", month: "short", day: "2-digit" })}: ${coin.current_price} ({(coin.price_change_24h<0?"▼":"▲") + '$' + Number(Math.abs(coin.price_change_24h))})"
+              classes='w-full rounded-box'
+              clickToDownload=true
+            />
+          </div>
         </div>
       {/if}
     </div>
