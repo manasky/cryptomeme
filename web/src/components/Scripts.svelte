53fab9bc5d2d9c0dd13ee849fa990418fd854794
Pouya Saadeghi <pouya.saadeghi@gmail.com>
2021-11-24 11:04:06 +0300

    add adsense code

--- web/src/components/Scripts.svelte
+++ web/src/components/Scripts.svelte
@@ -7,5 +7,8 @@
     gtag('js', new Date());
   
     gtag('config', 'UA-213457658-1');
-  </script>  
+  </script>
+
+  <!-- adsense -->
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4812562253949561" crossorigin="anonymous"></script>
 </svelte:head>
